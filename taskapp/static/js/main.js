c2b50ebf6739584d7dfc58719d760871a58079c1
Mike Norman <mike@mlnorman.com>
2014-10-04 06:17:41 -0400

    jquery added to addtask
    Signed-off-by: Mike Norman <mike@mlnorman.com>

--- taskapp/static/js/main.js
+++ taskapp/static/js/main.js
@@ -1,31 +1,61 @@
-/**
- * Created by admin on 9/20/2014.
- */
-$(function(){
-  $('#btnAgain').hide();
-  $('#btnReset').hide();
-  // capture search results
- 	$('.search').on('click', function(e){
- 		e.preventDefault();
-  	var parameters = { city: $('#city').val()};
-  	console.log(parameters)
-  	// send results to server
-    $.get( '/searching',parameters, function(data) {
-    	// handled returned data
-    	$('input').hide();
-    	$('#btnSearch').hide();
-    	$('#btnAgain').show();
-    	$('#btnReset').show();
-    	$('#results').html(data);
-  	});
- 	});
- 	// reset/start over
- 	$('#btnReset').on('click', function(e){
- 		e.preventDefault();
- 		$('input').val('').show();
-    $('#btnSearch').show();
-    $('#btnAgain').hide();
-    $('#btnReset').hide();
-    $('#results').html('');
-  });
+$(document).ready(function(){
+
+
+    $('form').on('submit', function() {
+
+        console.log("the form has beeen submitted");
+
+
+        var valueOne = $('input[name="task"]').val();
+        console.log(valueOne);
+
+        $.ajax({
+            type: "POST",
+            url: "/addTask/",
+            data : { task: valueOne },
+            success: function(results) {
+                var task_obj_len = results.tasks.length;
+                var del_task = "/delete_task/"+results.tasks[task_obj_len-1].task_id+'/';
+                $('#results').append('<div class="well in-task"><p class="the-task">'+results.tasks[task_obj_len-1].task+'</p>&nbsp;&nbsp;'+
+                '<a name="'+results.tasks[task_obj_len-1].task_id+'" href="'+del_task+'"><span class="glyphicon glyphicon-remove-circle"></span></a>&nbsp;&nbsp;</div>');
+                $('input').val('');
+
+            },
+            error: function(error) {
+              console.log(error)
+            }
+        });
+
+    });
+
+/*
+
+    $('a').on('click', function(){
+        var link_id = $(this).attr('name');
+        console.log(link_id);
+
+        $.ajax({
+            type: 'GET',
+            url: '/delete_task/' + link_id + '/',
+            success: function(del_results){
+                console.log(del_results);
+                var output = '';
+                for(var i = 0; i < del_results.tasks.length; i++){
+                    console.log(del_results.tasks[i].task);
+                    var del_task = "/delete_task/"+del_results.tasks[i].task_id+'/';
+                    output += '<div class="well in-task"><p class="the-task">'+del_results.tasks[i].task+'</p>&nbsp;&nbsp;'+
+                        '<a name="'+del_results.tasks[i].task_id+'" onclick="return false;" href="'+del_task+'"><span class="glyphicon glyphicon-remove-circle"></span></a>&nbsp;&nbsp;</div>';
+                    console.log(del_results.tasks[i].task);
+                }
+                //console.log(output);
+                $('#results').html(output);
+            },
+            error: function(error){
+                console.log(error);
+            }
+        });
+    });
+*/
+
+
 });
--- taskapp/views.py
+++ taskapp/views.py
@@ -1,6 +1,6 @@
 __author__ = 'Mike Norman'
 
-from flask import Flask, flash, redirect, url_for, request, render_template
+from flask import Flask, flash, redirect, url_for, request, render_template, jsonify, json
 from flask.ext.sqlalchemy import SQLAlchemy
 #from forms import CreateTaskForm
 
@@ -12,9 +12,9 @@ db = SQLAlchemy(app)
 from models import Task
 
 
-@app.route('/hello_world')
-def hello_world():
-    return 'Hello World'
+#@app.route('/hello_world')
+#def hello_world():
+#    return 'Hello World'
 
 @app.route('/')
 def index():
@@ -22,17 +22,45 @@ def index():
     return render_template("index.html", open_tasks=open_tasks)
 
 
-@app.route('/addTask/', methods=["POST"])
+@app.route('/addTask/', methods=["GET", "POST"])
 def add_task():
-    task = request.form['task']
-    if not task:
-        flash("You have to enter a task")
 
-        return redirect(url_for('index'))
-    else:
-        db.session.add(Task(task, 1))
-        db.session.commit()
-        return redirect(url_for('index'))
+    if request.method == "POST":
+        json_results = []
+        task = request.form.get('task')
+        if not task:
+            flash("You have to enter a task")
+            return redirect(url_for('index'))
+        else:
+            db.session.add(Task(task, 1))
+            db.session.commit()
+            open_tasks = db.session.query(Task).filter_by(status='1').order_by(Task.task_id.asc())
+            for item in open_tasks:
+                data = {
+                    'task_status': item.status,
+                    'task_id': item.task_id,
+                    'task': item.task
+                }
+                json_results.append(data)
+            return jsonify(tasks=json_results)
+           # return redirect(url_for('index'))
+
+
+
+#@app.route('/addTask/', methods=["POST"])
+#def add_task():
+#    task = request.form['task']
+#    if not task:
+#        flash("You have to enter a task")
+
+#        return redirect(url_for('index'))
+#    else:
+#        db.session.add(Task(task, 1))
+#        db.session.commit()
+#        return redirect(url_for('index'))
+
+
+
 
 
 @app.route('/delete_task/<int:task_id>/')
@@ -40,7 +68,7 @@ def delete_task(task_id):
     new_id = task_id
     db.session.query(Task).filter_by(task_id=new_id).delete()
     db.session.commit()
-    flash("Task was deleted!")
+    #flash("Task was deleted!")
     return redirect(url_for('index'))
 
 
@@ -50,15 +78,6 @@ def update_task(task_id):
     val = request.form[str(new_id)]
     db.session.query(Task).filter_by(task_id=new_id).update({'task': val})
     db.session.commit()
-    flash('Task was updated')
+    #flash('Task was updated')
     return redirect(url_for('index'))
 
-
-@app.route('/home')
-def home():
-    return render_template('home.html')
-
-
-@app.route('/test', methods=["POST"])
-def test():
-    return "yes"
